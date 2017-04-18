263c4673973f1520a7d880159299d1975efbae59
Jeff Guo <byguo2@yahoo.com>
2017-04-17 21:54:17 -0700

    Make AllowAnonymous work.

--- RisingWebApp/Controllers/HomeController.cs
+++ RisingWebApp/Controllers/HomeController.cs
@@ -5,12 +5,19 @@ using System.Web.Mvc;
 
 namespace RisingWebApp.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
         [AllowAnonymous]
         public ActionResult Index()
         {
             return View();
+        }
+
+        [AllowAnonymous]
+        public ActionResult Test()
+        {
+            return View("~/Views/Home/_Home.cshtml");
         }
     }
 }
